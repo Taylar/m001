143788a539773ff2a61937069e4693ed4352cf10
“zxt <302780150@qq.com>
2018-05-20 23:40:36 +0800

    none
    Signed-off-by: “zxt <302780150@qq.com>

--- src/main.c
+++ src/main.c
@@ -640,10 +640,10 @@ static void buttons_leds_init(bool * p_erase_bonds)
     uint32_t err_code = bsp_init(BSP_INIT_LEDS | BSP_INIT_BUTTONS, bsp_event_handler);
     APP_ERROR_CHECK(err_code);
 
-    err_code = bsp_btn_ble_init(NULL, &startup_event);
-    APP_ERROR_CHECK(err_code);
+    // err_code = bsp_btn_ble_init(NULL, &startup_event);
+    // APP_ERROR_CHECK(err_code);
 
-    *p_erase_bonds = (startup_event == BSP_EVENT_CLEAR_BONDING_DATA);
+    // *p_erase_bonds = (startup_event == BSP_EVENT_CLEAR_BONDING_DATA);
 }
 
 
